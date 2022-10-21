9e67c1e54e8ca5e0b54631223846734c3f5459ac
Hooopo Wang <hoooopo@gmail.com>
2022-10-21 13:40:40 +0800

    init meta info

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: discourse-asktug
+# about: Discourse plugin for asktug.com
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: 
+# url: https://github.com/hooopo/discourse-asktug
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :asktug_enabled
 
 after_initialize do
 end
