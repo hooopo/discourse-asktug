f4c964c93467f61d4df2295d6aa9416469186036
Hooopo Wang <hoooopo@gmail.com>
2022-10-21 15:19:41 +0800

    add fake email interceptor

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,12 @@
 # url: https://github.com/hooopo/discourse-asktug
 # required_version: 2.7.0
 
+PLUGIN_NAME ||= 'discourse-asktug'
+
 enabled_site_setting :asktug_enabled
 
 after_initialize do
+  [
+    "../config/initializers/fake_email_interceptor.rb"
+  ].each { |path| require File.expand_path(path, __FILE__) }
 end
