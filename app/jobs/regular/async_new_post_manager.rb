3e49beb86f00bb145f3a07397b17b78b092f4888
Hooopo Wang <hoooopo@gmail.com>
2022-11-10 22:44:49 +0800

    use id

--- app/jobs/regular/async_new_post_manager.rb
+++ app/jobs/regular/async_new_post_manager.rb
@@ -5,7 +5,8 @@ module Jobs
 
     def execute(opts)
       Rails.logger.info "start async group msg"
-      manager = NewPostManager.new(opts[:user], opts[:manager])
+      user = User.find(opts[:user])
+      manager = NewPostManager.new(user, opts[:manager])
       manager.perform
       Rails.logger.info "finish async group msg"
     end
